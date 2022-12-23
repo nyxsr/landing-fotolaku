aa4c5ad3da370188c1c4b7c22b386f6214e2b351
nyxsraid <sahrulramdan.75@gmail.com>
2022-12-23 21:12:08 +0700

    Adding Behavior on Ads

--- src/components/adsform/adsform.jsx
+++ src/components/adsform/adsform.jsx
@@ -3,10 +3,11 @@ import { AiOutlineClose } from "react-icons/ai";
 
 function Adsform() {
   const countdownDuration = 60;
-
   const [hours, setHours] = useState(0);
   const [minute, setMinute] = useState(0);
   const [second, setSecond] = useState(countdownDuration);
+
+  const [isShow, setShow] = useState(true);
 
   //   let countdownEndTime = Date.now("December 31, 2022 12:00:00");
 
@@ -17,36 +18,38 @@ function Adsform() {
 
     return () => clearInterval(countdownInterval);
   }, [second]);
-  return (
-    <section className="fixed bottom-0 bg-[#FD8703] py-3 px-3 z-50">
-      <div className="relative">
-        <div className="absolute right-0 top-0 text-white text-2xl">
-          <AiOutlineClose />
-        </div>
-        <div className="flex justify-center items-center gap-2">
-          <div className="bg-white py-2 px-2 rounded-lg">
-            <p>{hours}</p>
+  if (isShow === true) {
+    return (
+      <section className="fixed bottom-0 bg-[#FD8703] py-3 px-3 z-50">
+        <div className="relative">
+          <div className="absolute right-0 top-0 text-white text-2xl" onClick={()=>setShow(false)}>
+            <AiOutlineClose />
           </div>
-          <p>:</p>
-          <div className="bg-white py-2 px-2 rounded-lg">
-            <p>{minute}</p>
+          <div className="flex justify-center items-center gap-2">
+            <div className="bg-white py-2 px-2 rounded-lg">
+              <p>{hours}</p>
+            </div>
+            <p>:</p>
+            <div className="bg-white py-2 px-2 rounded-lg">
+              <p>{minute}</p>
+            </div>
+            <p>:</p>
+            <div className="bg-white py-2 px-2 rounded-lg">
+              <p>{second}</p>
+            </div>
           </div>
-          <p>:</p>
-          <div className="bg-white py-2 px-2 rounded-lg">
-            <p>{second}</p>
+          <p className="text-center text-xl py-5 text-white">
+            Daftar segera jika kamu ingin mendapatkan undian
+          </p>
+          <div className="flex justify-center">
+            <button className="bg-white py-[0.625rem] px-14 rounded-xl text-[#fd8703]" onClick={()=>window.location.href = 'https://fotolaku.com/landingpage/ads-form/'}>
+              Daftar Sekarang
+            </button>
           </div>
         </div>
-        <p className="text-center text-xl py-5 text-white">
-          Daftar segera jika kamu ingin mendapatkan undian
-        </p>
-        <div className="flex justify-center">
-          <button className="bg-white py-[0.625rem] px-14 rounded-xl text-[#fd8703]">
-            Daftar Sekarang
-          </button>
-        </div>
-      </div>
-    </section>
-  );
+      </section>
+    );
+  }
 }
 
 export default Adsform;
