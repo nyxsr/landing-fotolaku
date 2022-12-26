675dffbc54b8df1d1960da470f5cac2767a4c697
nyxsraid <sahrulramdan.75@gmail.com>
2022-12-26 14:00:00 +0700

    Adding social media onclick

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -28,10 +28,10 @@ function Hero() {
         <p className="text-[#747474] text-sm">with our creative consultant</p>
       </div>
       <div className="flex gap-[0.875rem] pt-4">
-        <div className="bg-[#FEF0CD] text-2xl text-[#fd8703] py-2 px-2 rounded-full">
+        <div className="bg-[#FEF0CD] text-2xl text-[#fd8703] py-2 px-2 rounded-full" onClick={()=>window.location.href = 'https://www.instagram.com/unmute.visual/'}>
             <FaInstagram/>
         </div>
-        <div className="bg-[#FEF0CD] text-2xl text-[#fd8703] py-2 px-2 rounded-full">
+        <div className="bg-[#FEF0CD] text-2xl text-[#fd8703] py-2 px-2 rounded-full" onClick={()=>window.location.href = 'https://www.tiktok.com/@foto.laku?_t=8YO2h0zNG7u&_r=1'}>
             <FaTiktok/>
         </div>
       </div>
