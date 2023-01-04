d753b59f9c0e2383122a271e7ac56c335fdd897e
nyxsraid <sahrulramdan.75@gmail.com>
2023-01-04 19:37:40 +0700

    Adding some minor changes

--- src/components/client/client.jsx
+++ src/components/client/client.jsx
@@ -1,20 +1,17 @@
-import React, { useRef, useState } from "react";
-import { FaPlay } from "react-icons/fa";
-import { AiOutlineStar,AiFillStar } from 'react-icons/ai'
+import React from "react";
 import { DataClient } from "../../data/data";
-import { Logo1 } from "../../assets/manage";
 
 function Client() {
   return (
-    <section className="pl-6">
-      <div className="flex flex-col justify-center items-center text-2xl font-semibold py-9">
+    <section className="">
+      <div className="flex pl-6 flex-col justify-center items-center text-2xl font-semibold py-9">
         <h1>LIHAT KATA</h1>
         <div className="relative bg-[#fd8703] text-white px-1 inline-block w-fit">
           CLIENT TENTANG KAMI
         </div>
       </div>
       <div className="overflow-x-scroll">
-        <div className="flex flex-nowrap py-6 gap-4 w-fit">
+        <div className="flex flex-nowrap py-6 gap-4 w-fit ml-6">
             {DataClient.map((v,i)=>{
                 return <Card key={i} id={i+1} brand={v.brand} text={v.text} />
             })}
@@ -27,10 +24,10 @@ function Client() {
 function Card(props) {
 
   return (
-    <div className="bg-white py-2 px-5 rounded-xl flex gap-2 items-center shadow-xl">
+    <div className="bg-white py-5 px-5 rounded-xl flex gap-2 items-center shadow-xl">
       <div className="relative w-[19.875rem] items-center justify-center flex flex-col rounded-xl">
        <blockquote className="text-center"><q>{props.text}</q></blockquote>
-       <p className="text-xl font-semibold my-5">{props.brand}</p>
+       <p className="text-xl font-semibold my-5 text-center">{props.brand}</p>
       </div>
     </div>
   );
